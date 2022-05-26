44c7e8e34676d9629720a06a6d771505ad2bdc70
AnthonyRo1 <anthonyseo556@gmail.com>
2022-05-26 13:38:26 -0700

    completed back and api route structure

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,17 +1,31 @@
+// Node Modules
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const {check} = require('express-validator');
 
+
+// Local Modules 
 const {setTokenCookie, restoreUser } = require('../../utils/auth');
+const { handleValidationErrors } = require('../../utils/validation');
 const { User } = require('../../db/models');
 
 
 // /api/session
 const router = express.Router();
 
-
+const validateLogin = [
+  check('credential')
+  .exists({checkFalsy: true})
+  .notEmpty()
+  .withMessage('Please provide a valid email or username'),
+  check('password')
+  .exists({checkFalsy: true} )
+  .withMessage('Please provide password'),
+  handleValidationErrors
+]
 
 // create a session upon user sign-up / login 
-router.post('/', asyncHandler(async (req, res, next) => {
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
   const {credential, password} = req.body;
 
   const user = await User.login({credential, password});
@@ -42,6 +56,9 @@ router.delete('/', (_req, res) => {
 });
 
 
+
+
+// restore user 
 router.get('/', restoreUser, (req, res) => {
   const {user} = req;
   if (user) {
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,18 +1,35 @@
+// Node Modules 
 const express= require('express');
 const asyncHandler = require('express-async-handler');
+const {check} = require('express-validator');
 
+// Local Modules 
 const {setTokenCookie, requireAuth} = require('../../utils/auth');
 const {User} = require('../../db/models');
-
+const { handleValidationErrors } = require('../../utils/validation');
 // /api/users
 const router = express.Router();
 
 
-
+const validateSignup = [
+  check('email')
+    .exists({checkFalsy: true})
+    .isEmail()
+    .withMessage('Please provide a valid email'),
+  check('username')
+    .exists({checkFalsy: true})
+    .isLength({min: 4})
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('password')
+  .exists({checkFalsy: true})
+  .isLength({min: 6})
+  .withMessage('Password must be 6 characters or more'),
+  handleValidationErrors
+]
 
 
 // sign up user 
-router.post('/', asyncHandler( async(req, res) => {
+router.post('/', validateSignup, asyncHandler( async(req, res) => {
   const {email, username, password} = req.body;
 
   const user = await User.signup({ email, username, password});
